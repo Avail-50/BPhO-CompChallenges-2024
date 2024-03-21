8e2044ed5b458c025b12e66b16110edb4afbdd84
Avail-50 <138884615+Avail-50@users.noreply.github.com>
2024-03-21 22:17:38 +0000

    suvat

--- Challenge1.py
+++ Challenge1.py
@@ -7,14 +7,23 @@ class Model():
         self.g = gravity       
         self.ux, self.uy = self.resolve(launchSpeed)
         self.h = launchHeight
+        self.ypos = []
+
 
     def resolve(self, u):
         uy = np.sin(self.launchAngle) * u
         ux = np.cos(self.launchAngle) * u
         return(ux, uy)
 
-    def suvat(self):
-        pass
+    def suvat(self, time: float):
+        s = self.uy * time - (self.g * time**2)/2 + self.h
+        if s >= 0:
+            self.ypos.append(s)
+            return time
+        else:
+            return None
+
+TIMEPERIOD = 0.1
 
 '''
 
@@ -22,8 +31,24 @@ class Model():
 / |
 ---
 
+s = (ut + 1/2 *a *t**2) + h
+
+
 '''
 
-mod = Model(0, 9.81, 5, 0)
+mod = Model(90, 9.81, 5, 0)
 print(mod.ux)
 print(mod.uy)
+
+time = 0
+
+time = mod.suvat(time) 
+
+while time != None:
+    time += TIMEPERIOD
+    time = mod.suvat(time)   
+    print(time)
+
+
+print(mod.ypos)
+
