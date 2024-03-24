ebea8f22525c5c7104d4c833ecf95c7edfa2f1e2
Avail-50 <138884615+Avail-50@users.noreply.github.com>
2024-03-24 16:23:31 +0000

    UI and matplotlib working

--- Challenge1.py
+++ Challenge1.py
@@ -10,14 +10,14 @@ from matplotlib.figure import Figure
 
 
 class Projectile():
-    def __init__(self, launchAngle, gravity, launchSpeed, launchHeight) -> None:
+    def __init__(self, launchAngle, gravity, launchSpeed, launchHeight, timePeriod) -> None:
         self.launchAngle = launchAngle * np.pi/180
         self.g = gravity       
         self.ux, self.uy = self.resolve(launchSpeed)
         self.h = launchHeight
         self.ypos = []
         self.xpos = []
-        self.timePeriod = 0.1
+        self.timePeriod = timePeriod
 
     def resolve(self, u):
         uy = np.round(np.sin(self.launchAngle) * u, 2)
@@ -44,29 +44,15 @@ class Projectile():
             time += self.timePeriod
             time = self.suvat(time) 
       
-mod = Projectile(50, 9.81, 20, 4)
-mod.simulate()
-
-'''
-def graph(x: list, y: list):
-    plt.plot(x, y)
-    plt.title("Distance of Projectile")
-    plt.xlabel("x /m")
-    plt.ylabel("y /m")
-    plt.show()
-
-graph(mod.xpos, mod.ypos)
-'''
-
-
-
+#mod = Projectile(50, 9.81, 20, 4)
+#mod.simulate()
 
 mw = tk.Tk()
 mw.title("Projectile: No Air Resistance")
 
 fig = Figure(figsize=(6, 6), dpi= 100)
 ax = fig.add_subplot()
-graph = ax.plot(mod.xpos, mod.ypos)
+
 ax.set_xlabel("x /m")
 ax.set_ylabel("y /m")
 
@@ -76,48 +62,42 @@ canvas.draw()
 toolbar = NavigationToolbar2Tk(canvas, mw, pack_toolbar=False)
 toolbar.update()
 
+'''
 canvas.mpl_connect(
     "key_press_event", lambda event: print(f"you pressed {event.key}"))
 canvas.mpl_connect("key_press_event", key_press_handler)
+'''
+
 button_quit = tk.Button(master=mw, text="Quit", command=mw.destroy)
-
-'''
-def update_frequency(new_val):
-    # retrieve frequency
-    f = float(new_val)
-
-    # update data
-    y = 2 * np.sin(2 * np.pi * f * t)
-    line.set_data(t, y)
-
-    # required to update canvas and attached toolbar!
-    canvas.draw()
-
-
-slider_update = tkinter.Scale(root, from_=1, to=5, orient=tkinter.HORIZONTAL,
-                              command=update_frequency, label="Frequency [Hz]")
-'''
 
 launchAngle_var = tk.IntVar()
 gravity_var = tk.IntVar()
 launchSpeed_var = tk.IntVar()
 launchHieght_var = tk.IntVar()
+####
+timePeriod_var = tk.StringVar()
+####
 
 def submit():
     angle = launchAngle_var.get()
     grav = gravity_var.get()
     speed = launchSpeed_var.get()
     height = launchHieght_var.get()
+    tP = timePeriod_var.get()
 
-    mod = Projectile(angle, grav, speed, height)
+    mod = Projectile(angle, grav, speed, height, float(tP))
     mod.simulate()
-    graph = ax.plot(mod.xpos, mod.ypos)
+    ax.clear()
+    ax.plot(mod.xpos, mod.ypos)
     canvas.draw()
 
 launchAngle_label, launchAngle_entry = tk.Label(mw, text = 'Launch Angle', font=('calibre',10, 'bold')), tk.Entry(mw,textvariable = launchAngle_var, font=('calibre',10,'normal'))
 gravity_label, gravity_entry = tk.Label(mw, text = 'Gravity', font=('calibre',10, 'bold')), tk.Entry(mw,textvariable = gravity_var, font=('calibre',10,'normal'))
 launchSpeed_label, launchSpeed_entry = tk.Label(mw, text = 'Launch Speed', font=('calibre',10, 'bold')), tk.Entry(mw,textvariable = launchSpeed_var, font=('calibre',10,'normal'))
 launchHeight_label, launchHeight_entry = tk.Label(mw, text = 'Launch Height', font=('calibre',10, 'bold')), tk.Entry(mw,textvariable = launchHieght_var, font=('calibre',10,'normal'))
+
+tP_label, tP_entry = tk.Label(mw, text = 'Time Period', font=('calibre',10, 'bold')), tk.Entry(mw,textvariable = timePeriod_var, font=('calibre',10,'normal'))
+
 sub_btn=tk.Button(mw, text = 'Submit', command = submit)
 
 
@@ -129,6 +109,10 @@ launchSpeed_label.pack(side=tk.TOP)
 launchSpeed_entry.pack(side=tk.TOP)
 launchHeight_label.pack(side=tk.TOP)
 launchHeight_entry.pack(side=tk.TOP)
+
+tP_label.pack(side=tk.TOP)
+tP_entry.pack(side=tk.TOP)
+
 sub_btn.pack(side=tk.TOP)
 
 
