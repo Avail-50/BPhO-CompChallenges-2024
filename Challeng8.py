223e356fc7e58da3cb32b38df9b9f70ed2a0881f
Avail-50 <138884615+Avail-50@users.noreply.github.com>
2024-08-05 20:44:44 +0100

    Challenge 8 animation done

--- Challeng8.py
+++ Challeng8.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 class BouncingProjectile():
-    def __init__(self, launchAngle, launchSpeed, launchHeight, n, g=9.81, freq=250):
+    def __init__(self, launchAngle, launchSpeed, launchHeight, n, g=9.81, freq=100):
         self.theta = launchAngle * np.pi/180
         self.ux, self.uy = self.resolve(launchSpeed)
         self.y_0 = launchHeight
@@ -36,14 +37,51 @@ class BouncingProjectile():
                 bounceN += 1
         
         return x_coords, y_coords, t
+
+'''
+def update(frame):
+    x = plot[0][:frame]
+    y = plot[1][:frame]
+    
+    anim.set_xdata(plot[0][:frame])
+    anim.set_ydata(plot[1][:frame])
+
+    return anim
+
+
         
 bounce = BouncingProjectile(45, 5, 10, 6)
 plot = bounce.verlet()
-plt.plot(plot[0], plot[1], "r-", label="y vs x")
-print(plot[2])
+fig, ax = plt.subplots()
+anim = ax.plot(plot[0][0], plot[1][0])
+#plt.plot(plot[0], plot[1], "r-", label="y vs x")
 
 
+ani = animation.FuncAnimation(fig=fig, func=update, frames=40, interval=30)
 plt.show()
+'''
+
+fig, ax = plt.subplots()
+bounce = BouncingProjectile(45, 5, 10, 6)
+plotx, ploty, t = bounce.verlet()
+line2 = ax.plot(plotx[0], ploty[0], "r-")[0]
+ball = ax.plot(plotx[0], ploty[0], "go")[0]
+
+ax.set(xlim=[0, 40], ylim=[0, 15], xlabel='Time [s]', ylabel='Z [m]')
+#ax.legend()
+
+def update(frame):
+
+    # update the line plot:
+    line2.set_xdata(plotx[:frame])
+    line2.set_ydata(ploty[:frame])
+
+    ball.set_xdata(plotx[frame])
+    ball.set_ydata(ploty[frame])
+    
+    return (line2)
 
 
+ani = animation.FuncAnimation(fig=fig, func=update, frames=len(plotx), interval = 10)
+plt.show()
 
